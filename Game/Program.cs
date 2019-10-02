f4bccb0cb3d5342478dba3c1100469e82c257e5c
Fenex <31255152+thumpnail@users.noreply.github.com>
2019-10-02 12:09:00 +0200

    Add files via upload

---
+++ Game/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Engine;
+
+namespace BaseGame {
+    class Program {
+        //uses/needs the 'Engine' to work
+        static void Main(string[] args) {
+
+        }
+    }
+}
