ac136da8fd27e606bf93ea1c2769d9c0d0413f2a
thumbnail <friedewald24@gmail.com>
2019-10-02 13:25:13 +0200

    boi das gea

--- Engine/Core.cs
+++ Engine/Core.cs
@@ -1,48 +1,10 @@
 ﻿using System;
 
-/*
- * Namespaces:
- * -Engine(Base Stuff)
- * --Core(Configure and start)
- * --Data(Static varibles like version, etc)
- * -Engine.Game(Main loop if needed)
- * -Engine.Game.Command
- * -Engine.Game.GameConsole
- * -Engine.Entities
- * -Engine.Utilities (Generation and Functions that may be usefull)
- * -Engine.Utilities.GamePlay
- * -Engine.Utilities.Loot
- *(-Engine.Utilities.Loot.Items)
- * -Engine.Utilities.Map
- * -Engine.Utilities.Room
- * -Engine.Utilities.Story
- * -Engine.Utilities.Economic
- * -Engine.Utilities.File(ini, xml, mby db)
- * -Engine.Utilities.Threading
- * -Engine.Utilities.Additionals
- * -Engine.Utilities.Crypto
- * -Engine.Inventory
-*/
-
 namespace Engine {
-    //Configure and Start
+    public class Core {
 
-    /*
-     *  - Text Based RPG Engine
-     *  - Round Based RPG
-     *  - Rules
-     * 0. Command Controls(mby hotkeys)
-     * 1. Inventar System
-     * 2. Combat System(Enemies, stats, etc.)
-     * 3. Loot System(Random)
-     * 4. Story Support System(Story)
-     * 5. Potion System (siehe Potion trinken und nen Cooldown)
-     * 6. Map(over file)/Virtual Rooms(mby both? , Lootable/Collactable Entities[Chests, Herbs 'n' Stuff]),(maybe Captureable Monsters/Pets)
-     * 7. items with xml/ini file
-     * 8. Money
-     * 9. open world/story driven(tunnel game)
-     * 10.(Easter eggs)
-     * 11.Range and 
-     * 12.
-    */
+    }
+    public class Data {
+        public static string version = "v0.1";
+    }
 }
