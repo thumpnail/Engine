097131ffc4663d23bbbde6b7aad9ee00d886c562
thumpnail <friedewald24@gmail.com>
2019-10-18 09:05:54 +0200

    .

---
+++ Engine/Chars/Player.cs
@@ -0,0 +1,15 @@
+using System;
+using Engine;
+using Engine.Inventory;
+
+namespace Engine.Chars {
+    public class Player {
+        public InventoryManager InvManager;
+        public string name;
+        public string hp;
+        public Item equiptedItem;
+        public Player() {
+            
+        }
+    }
+}
---
+++ Engine/Inventory/Item.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Engine.Inventory
+{
+    public class Item
+    {
+    }
+}
